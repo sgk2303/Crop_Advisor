0ed706db5b4378f2dab665e79629019ea969b7bc
sgk2303 <gkvv2303@gmail.com>
2023-03-11 02:47:19 +0530

    Updated Folder Structure

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import numpy as np
 import config
 import requests
 from utils.disease import disease_dic
-from utils.fertilizer import fertilizer_dic
 import requests
 import config
 import pickle
@@ -18,44 +17,7 @@ app = Flask(__name__)
 
 classes=pickle.load(open('models/crop_classes.pkl','rb'))
 
-disease_classes = ['Apple___Apple_scab',
-                'Apple___Black_rot',
-                'Apple___Cedar_apple_rust',
-                'Apple___healthy',
-                'Blueberry___healthy',
-                'Cherry_(including_sour)___Powdery_mildew',
-                'Cherry_(including_sour)___healthy',
-                'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
-                'Corn_(maize)___Common_rust_',
-                'Corn_(maize)___Northern_Leaf_Blight',
-                'Corn_(maize)___healthy',
-                'Grape___Black_rot',
-                'Grape___Esca_(Black_Measles)',
-                'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
-                'Grape___healthy',
-                'Orange___Haunglongbing_(Citrus_greening)',
-                'Peach___Bacterial_spot',
-                'Peach___healthy',
-                'Pepper,_bell___Bacterial_spot',
-                'Pepper,_bell___healthy',
-                'Potato___Early_blight',
-                'Potato___Late_blight',
-                'Potato___healthy',
-                'Raspberry___healthy',
-                'Soybean___healthy',
-                'Squash___Powdery_mildew',
-                'Strawberry___Leaf_scorch',
-                'Strawberry___healthy',
-                'Tomato___Bacterial_spot',
-                'Tomato___Early_blight',
-                'Tomato___Late_blight',
-                'Tomato___Leaf_Mold',
-                'Tomato___Septoria_leaf_spot',
-                'Tomato___Spider_mites Two-spotted_spider_mite',
-                'Tomato___Target_Spot',
-                'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
-                'Tomato___Tomato_mosaic_virus',
-                'Tomato___healthy']
+disease_classes = pickle.load(open('models/disease_classes.pkl','rb'))
 
 disease_model_path = 'models/plant_disease_model.pth'
 disease_model = ResNet9(3, len(disease_classes))
@@ -64,11 +26,6 @@ disease_model.load_state_dict(torch.load(
 disease_model.eval()
 
 def predict_image(img, model=disease_model):
-    """
-    Transforms image to tensor and predicts disease label
-    :params: image
-    :return: prediction (string)
-    """
     transform = transforms.Compose([
         transforms.Resize(256),
         transforms.ToTensor(),
@@ -77,23 +34,15 @@ def predict_image(img, model=disease_model):
     img_t = transform(image)
     img_u = torch.unsqueeze(img_t, 0)
 
-    # Get predictions from model
     yb = model(img_u)
-    # Pick index with highest probability
     _, preds = torch.max(yb, dim=1)
     prediction = disease_classes[preds[0].item()]
-    # Retrieve the class label
     return prediction
 
 
 print(classes)
 
 def weather_fetch(city_name):
-    """
-    Fetch and returns the temperature and humidity of a city
-    :params: city_name
-    :return: temperature, humidity
-    """
     print(city_name)
     api_key = config.weather_api_key
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
@@ -131,9 +80,7 @@ def app_data():
             return render_template('disease.html')
         try:
             img = file.read()
-
             prediction = predict_image(img)
-
             prediction = Markup(str(disease_dic[prediction]))
             print(prediction)
             return render_template('disease-result.html', predictions=prediction)
@@ -145,12 +92,12 @@ def app_data():
 @app.route('/recform', methods=['POST'])
 def regform():
     N = request.form['N']
-    P = request.form['P']       #Selected contact id type
+    P = request.form['P']       
     K = request.form['K']
     city = request.form['city']
     print(city)
     temperature, humidity = weather_fetch(city)
-    ph = request.form['ph']       #Selected contact id type
+    ph = request.form['ph']       
     rainfall = request.form['rainfall']
     data=[[N,P,K,temperature,humidity,ph,rainfall]]
     model=pickle.load(open('models/crop_recomender.pkl','rb'))
